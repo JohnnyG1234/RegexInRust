de5688b22ee32412799fa80e16b251fe42238dc8
John Groton <john.groton@mymail.champlain.edu>
2025-03-05 19:28:44 -0500

    getting first char

--- regex/src/main.rs
+++ regex/src/main.rs
@@ -1,7 +1,17 @@
+use std::ptr::null;
+
 fn main() {
-    parse_range_quantifier();
+    parse_range_quantifier("hello".to_string());
 }
 
-fn parse_range_quantifier() {
-    println!("yo")
+fn parse_range_quantifier(sub_string: String) {
+    let mut to_parse: String = sub_string;
+
+    println!("{}", to_parse.chars().next().unwrap());
+    if to_parse.chars().next().unwrap() != '{'
+    {
+        println!("{}", "Not Valid Regex!");
+    }
+    to_parse.pop();
+    
 }
