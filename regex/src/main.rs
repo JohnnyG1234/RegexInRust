e90db762fed84459c62582b420c6c56c1e94d8e0
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2025-03-08 14:46:28 -0500

    Rust does not like me :(

--- regex/src/main.rs
+++ regex/src/main.rs
@@ -1,6 +1,6 @@
 
 fn main() {
-    parse_range("?");
+    parse_range(".?");
 }
 
 
@@ -11,19 +11,40 @@ struct Token{
 
 
 fn parse_range(sub_string: &str) {
-    let mut  _v: Vec<Token> = Vec::new();
-    let default: Token = Token{tag: "NULL".to_string(), quantifier: "NULL".to_string()};
+    let mut  _v: Vec<Vec<Token>> = Vec::new();
+    let mut default: Token = Token{tag: "NULL".to_string(), quantifier: "NULL".to_string()};
 
     for i in sub_string.chars() {   
         match i {
            '.' => {
-                _v.push(Token {tag: "wildcard".to_string(), quantifier: "exactlyOne".to_string()});
+                _v.last_mut().unwrap().push(Token {tag: "wildcard".to_string(), quantifier: "exactlyOne".to_string()});
            },
            '?' => {
-                let _last: &Token = _v.last().unwrap_or(&default);
+                let _last: &mut Token = _v.last_mut().unwrap().last_mut().unwrap_or(&mut default);
                 if _last.quantifier != "exactlyOne" {
-                    println!("Error!!!")
+                    println!("Error!!!");
+                    return;
                 }
+
+                _last.quantifier = "exactlyOne".to_string();
+           },
+           '*' => {
+                let _last: &mut Token = _v.last_mut().unwrap().last_mut().unwrap_or(&mut default);
+                if _last.quantifier != "exactlyOne" {
+                    println!("Error!!!");
+                    return;
+                }
+
+                _last.quantifier = "zeroOrMore".to_string();
+           },
+           '+' => {
+                let _last: &Token = _v.last().unwrap().last().unwrap_or(&default);
+                if _last.quantifier != "exactlyOne" {
+                    println!("Error!!!");
+                    return;
+                }
+
+                _v.last_mut().unwrap().push(Token {tag: _last.tag.clone(), quantifier: "zeroOrMore".to_string()});
            },
            _ => println!("{}", "default case")
         }
